ea44e330a93e9b21ef7674b14af9af59987c5422
Brayden Seaman <braydenseaman@gmail.com>
2025-02-26 00:33:06 -0500

    Another syntax

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 # Allow CORS for frontend access.
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=True,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
